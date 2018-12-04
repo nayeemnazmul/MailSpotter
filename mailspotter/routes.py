2e4b6e9e797347558d9480630aedc85cfaee6981
nayeemnazmul <md.nazmul@northsouth.edu>
2018-12-04 12:17:04 +0600

    moved save_picture method under account route

--- mailspotter/routes.py
+++ mailspotter/routes.py
@@ -49,20 +49,6 @@ def logout():
 	return redirect(url_for('dashboard'))
 
 
-def save_picture(form_picture):
-	random_hex = secrets.token_hex(8)
-	_, f_ext = os.path.splitext(form_picture.filename)
-	picture_fn = random_hex + f_ext
-	picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-
-	output_size = (125, 125)
-	i = Image.open(form_picture)
-	i.thumbnail(output_size)
-	i.save(picture_path)
-
-	return picture_fn
-
-
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
@@ -82,3 +68,17 @@ def account():
 
 	image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
 	return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+def save_picture(form_picture):
+	random_hex = secrets.token_hex(8)
+	_, f_ext = os.path.splitext(form_picture.filename)
+	picture_fn = random_hex + f_ext
+	picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+	output_size = (125, 125)
+	i = Image.open(form_picture)
+	i.thumbnail(output_size)
+	i.save(picture_path)
+
+	return picture_fn
